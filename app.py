974bc9476b1bb6a8ad0312b460cfdf5705882f80
@johnya0 <anoopjohny2000@gmail.com>
2023-06-05 12:25:26 +0200

    version

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 import streamlit as st
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
-from langchain.agents import AgentType
-from langchain.llms import OpenAI
-import os
+from langchain.utilities import WikipediaAPIWrapper
 
-from apikey import apikey
 
-os.environ["--------"] = apikey
 
-llm = OpenAI(temperature=0)
-tools = load_tools(['wikipedia'], llm=llm)
-agent = initialize_agent(tools, llm, AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+st.title("Streamlit Langchain App : 🦜")
+input =st.text_input('Prompt>>> ')
+wikipedia =WikipediaAPIWrapper()  ## current tool set
 
-input_prompt = st.text_input('Prompt>>> ')
-
-if input_prompt:
-    text = agent.run(input_prompt)
-    st.text(text)
+if input :
+    text = wikipedia.run(input)
+    st.text_area(text)
