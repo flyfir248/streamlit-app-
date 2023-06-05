ca9d518dfa00997a4ec03512282250ee78fa01c5
@johnya0 <anoopjohny2000@gmail.com>
2023-06-05 17:17:15 +0200

    version

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+from transformers import AutoTokenizer, pipeline
 from langchain.utilities import WikipediaAPIWrapper
+import torch
 import streamlit as st
 
 st.title("Streamlit Question Answering App 🦜 🦚")
 
-# Load the question answering model and tokenizer
-model_name = "deepset/roberta-base-squad2"
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+# Load the tokenizer for Falcon LLM
+model_name = "tiiuae/falcon-40b"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# Create a pipeline for question answering
-nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+# Create a pipeline for text generation using Falcon LLM
+pipeline_falcon = pipeline(
+    "text-generation",
+    model=model_name,
+    tokenizer=model_name,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+)
 
 # User input
 question_input = st.text_input("Question:")
@@ -23,15 +30,19 @@ if question_input:
     wikipedia = WikipediaAPIWrapper()
     context_input = wikipedia.run(' '.join(keywords))
 
-    # Prepare the question and context for question answering
-    QA_input = {
-        'question': question_input,
-        'context': context_input
-    }
+    # Generate extended answer using Falcon LLM
+    prompt = f"{context_input}\nQuestion: {question_input}\nAnswer:"
+    sequences = pipeline_falcon(
+        prompt,
+        max_length=200,  # Adjust the max length as per your requirement
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+    )
 
-    # Get the answer using the question answering pipeline
-    res = nlp(QA_input, max_length=500)  # Adjust max_length as per your requirement
+    # Extract the generated text from Falcon LLM output
+    generated_text = sequences[0]['generated_text'].replace(prompt, "").strip()
 
     # Display the answer
-    st.text_area("Answer:", res['answer'])
-    st.write("Score:", res['score'])
+    st.text_area("Answer:", generated_text)
